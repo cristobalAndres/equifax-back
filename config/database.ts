bfc5baf6a33e2a08eea6eddf0354266898e8d0b2
Cristobal Palma <cristobalpalma@MacBook-Pro-Cristobal.local>
2022-06-15 20:06:29 -0400

    add-user-and-tickets

--- config/database.ts
+++ config/database.ts
@@ -3,14 +3,27 @@ import { Sequelize, BuildOptions, Model } from 'sequelize';
 
 let sequelize: Sequelize;
 
-sequelize = new Sequelize({
-  database: process.env.DATABASE,
-  dialect: 'mysql',
-  host: process.env.HOST,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '3306', 10),
-  username: process.env.DB_USERNAME,
-});
+if (process.env.NODE_ENV === 'dev') {
+  sequelize = new Sequelize({
+    database: process.env.DATABASE,
+    dialect: 'mysql',
+    host: process.env.HOST,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    username: process.env.DB_USERNAME,
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: `/cloudsql/${process.env.CLOUD_SQL}`,
+    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME,
+    database: process.env.DATABASE,
+    dialectOptions: {
+      socketPath: `/cloudsql/${process.env.CLOUD_SQL}`,
+    },
+  });
+}
 
 export { sequelize };
 
